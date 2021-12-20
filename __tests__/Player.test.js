c09dea1279fdbeede99b092ceca3c46abaefd8a1
Mark Dale <msdaledad@gmail.com>
2021-12-19 20:11:33 -0500

    updated with 10.2.zip

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,17 +1,17 @@
-const Player = require('../lib/Player');
-const Potion = require('../lib/Potion');
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
 
-jest.mock('../lib/Potion');
+jest.mock('../lib/Potion.js');
 
 test('creates a player object', () => {
   const player = new Player('Dave');
-  expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-  );
+
   expect(player.name).toBe('Dave');
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
 test("gets player's stats as an object", () => {
